ab3ed1d1d7c5fb10fa929197b3e811e20f39b946
Jay Holland <jaycholland@gmail.com>
2018-09-11 01:32:19 -0700

    my test answers

--- code.js
+++ code.js
@@ -1,17 +1,31 @@
 var code = {
     // Returns "Hello World!"
     helloWorld: function() {
-        throw new Error("Not Implemented");
+        return "Hello World!";
     },
 
     // Take a single-spaced <sentence>, and capitalize every <n>th word starting with <offset>.
     capitalizeEveryNthWord: function(sentence, offset, n) {
-        throw new Error("Not Implemented");
+        let words = sentence.split(" ");
+        for(let i = offset; i < words.length; i++){
+            if((i - offset) % n  === 0){
+                words[i] = words[i][0].toUpperCase() + words[i].substring(1);
+            }
+        }
+        return words.join(" ");
     },
     
     // Determine if a number is prime
     isPrime: function(n) {
-        throw new Error("Not Implemented");
+        if(n <= 1) return false;
+        if(n <= 3) return true;
+        if(n % 2 === 0 || n % 3 === 0) return false; 
+        for(let i = 5; n >= i * i; i += 6){
+            if (n % i === 0 || n % (i + 2) === 0){
+                return false;
+            }
+        }
+        return true;
     },
     
     // Calculate the golden ratio.
@@ -21,20 +35,51 @@ var code = {
     // Let e = c + d, then the ratio e / d is closer to the golden ratio.
     // If you continue this process, the result will trend towards the golden ratio.
     goldenRatio: function(a, b) {
-        throw new Error("Not Implemented");
+        let nums = [a,b];
+        for(let i = 0; i < 1000; i++){
+            nums.push(nums[i] + nums[i+1]);
+        }
+        return nums[nums.length - 1] / nums[nums.length - 2];
     },
 
     // Give the nth Fibonacci number
     // Starting with 0, 1, 1, 2, ... a Fibonacci number is the sum of the previous two.
     fibonacci: function(n) {
-        throw new Error("Not Implemented");
+        if(n === 0) return 0;
+        let fibSequence = [0,1];
+        for(let i = 1; i < n; i++){
+            fibSequence.push(fibSequence[i-1] + fibSequence[i]);
+        }
+        return fibSequence.pop();
     },
 
     // Give the square root of a number
     // Using a binary search algorithm, search for the square root of a given number.
     // Do not use the built-in square root function.
     squareRoot: function(n) {
-        throw new Error("Not Implemented");
+        if(n === 0) return 0;
+        if(n === 1) return 1;
+        if(n < 0) return "imaginary";
+        let low = 0;
+        let high = n;
+        if(n < 1) high = 1;
+        let mid = high / 2;
+        let epsilon = .00000000000001; // reduced Number.EPSILON for acceptable margin of error
+        let count = 0;
+        while(count < 100000000){
+            let test = mid * mid;
+            if(Math.abs(test - n) < epsilon){ // test for equality within precision of epsilon
+                return mid;
+            } else if(test > n){
+                high = mid; // move high down
+                mid -= (mid - low) / 2;
+            } else {
+                low = mid; // move low up
+                mid += (high - mid) / 2;
+            }
+            count++;
+        }
+        return mid; // max loop count force return of current mid value
     }
 };
 module.exports = code;
