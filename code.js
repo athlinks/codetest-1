502419346daffd3444aad850c1ae2091fc1ddbf1
Christopher Dierkens <cjdierkens@gmail.com>
2018-09-12 00:27:30 -0400

    Implement JavaScript code

--- code.js
+++ code.js
@@ -1,17 +1,30 @@
 var code = {
     // Returns "Hello World!"
     helloWorld: function() {
-        throw new Error("Not Implemented");
+        return "Hello World!"
     },
 
     // Take a single-spaced <sentence>, and capitalize every <n>th word starting with <offset>.
     capitalizeEveryNthWord: function(sentence, offset, n) {
-        throw new Error("Not Implemented");
+        const words = sentence.split(" ")
+        for (let i = offset; i < words.length; i++) {
+            if (i % n === 0) {
+                const chars = words[i].split("");
+                chars[0] = chars[0].toUpperCase();
+                words[i] = chars.join("");
+            }
+        }
+        return words.join(" ")
     },
     
     // Determine if a number is prime
     isPrime: function(n) {
-        throw new Error("Not Implemented");
+        for (let i = 2; i < n; i++) {
+            if (n % i === 0) {
+                return false;
+            }
+        }
+        return n > 1;
     },
     
     // Calculate the golden ratio.
@@ -21,20 +34,53 @@ var code = {
     // Let e = c + d, then the ratio e / d is closer to the golden ratio.
     // If you continue this process, the result will trend towards the golden ratio.
     goldenRatio: function(a, b) {
-        throw new Error("Not Implemented");
+        do {
+            var ratio = (a + b) / b;
+            a = b;
+            b = (a + b);
+        } while (1.61800 > ratio && ratio < 1.61806)
     },
 
     // Give the nth Fibonacci number
     // Starting with 0, 1, 1, 2, ... a Fibonacci number is the sum of the previous two.
     fibonacci: function(n) {
-        throw new Error("Not Implemented");
+        let fib = [];
+
+        fib[0] = 0;
+        fib[1] = 1;
+
+        for (let i = 2; i <= n; i+=1) {
+            fib[i] = fib[i-2] + fib[i-1];
+        }
+        
+        return fib[n]
     },
 
     // Give the square root of a number
     // Using a binary search algorithm, search for the square root of a given number.
     // Do not use the built-in square root function.
     squareRoot: function(n) {
-        throw new Error("Not Implemented");
+        let ceil = n;
+        let floor = 0; 
+    
+        let root = (ceil - floor) / 2;
+        let squared = root * root;
+    
+        while (Math.abs(squared - n) > 0.00000000000001) {
+            
+            if (squared > n) {
+                ceil = root;
+                root -= (ceil - floor) / 2;
+            } else {
+                floor = root;
+                root += (ceil - floor) / 2;
+    
+            }
+    
+            squared = root * root;
+    
+        }
+        return root;
     }
 };
 module.exports = code;
